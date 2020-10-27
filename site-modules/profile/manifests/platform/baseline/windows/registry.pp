6e551bc4e7fdd3f812e458b7b1d5ca3c49c4aed9
jameslanepuppet <james.lane@puppet.com>
2020-10-27 19:19:06 +0000

    updated registry.pp

--- site-modules/profile/manifests/platform/baseline/windows/registry.pp
+++ site-modules/profile/manifests/platform/baseline/windows/registry.pp
@@ -1,12 +1,10 @@
 #requires puppetlabs/registry
 
 class profile::platform::baseline::windows::registry {
-
- # registry_value { 'Enable IE ESC for Administrators':
- #   path   => 'HKEY_LOCAL_MACHINE/SOFTWARE/Microsoft/Active Setup/Installed Components/{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}',
- #   ensure => present,
- # }
-  registry_key { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability':
+ 
+  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability':
     ensure => present,
+    data  => '1',
+    type  => 'dword',
   }
-}
+
