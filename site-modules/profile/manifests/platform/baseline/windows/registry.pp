1e28d54edfb19e2ffe97b24565603ad5cba5f01f
jameslanepuppet <james.lane@puppet.com>
2020-10-27 18:43:43 +0000

    updated registry.pp

--- site-modules/profile/manifests/platform/baseline/windows/registry.pp
+++ site-modules/profile/manifests/platform/baseline/windows/registry.pp
@@ -2,7 +2,7 @@
 
 class profile::platform::baseline::windows::registry {
 
-  registry::value { 'Enable IE ESC for Administrators':
+  registry_value { 'Enable IE ESC for Administrators':
     key   => 'HKEY_LOCAL_MACHINE/SOFTWARE/Microsoft/Active Setup/Installed Components/{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}',
     ensure => present,
   }
