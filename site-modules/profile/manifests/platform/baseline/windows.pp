4b85fc208a08f2596da8a730ebf634c5145fee16
jameslanepuppet <james.lane@puppet.com>
2020-10-27 19:38:42 +0000

    declared iis.pp

--- site-modules/profile/manifests/platform/baseline/windows.pp
+++ site-modules/profile/manifests/platform/baseline/windows.pp
@@ -3,5 +3,5 @@ class profile::platform::baseline::windows {
   include ::profile::platform::baseline::users::windows
   include ::profile::platform::baseline::windows::directories
   include ::profile::platform::baseline::windows::registry
-
+  include ::profile::platform::baseline::windows::iis
 }
--- site-modules/profile/manifests/platform/baseline/windows/iis.pp
+++ site-modules/profile/manifests/platform/baseline/windows/iis.pp
@@ -4,4 +4,22 @@ class profile::platform::baseline::windows::iis {
     ensure => present,
     answer => 'C:\answer\iis.xml',
   }
+  iis_application_pool { 'minimal_site_app_pool':
+    ensure                  => 'present',
+    state                   => 'started',
+    managed_pipeline_mode   => 'Integrated',
+    managed_runtime_version => 'v4.0',
+  } ->
+
+  iis_site { 'minimal':
+    ensure          => 'started',
+    physicalpath    => 'c:\\inetpub\\minimal',
+    applicationpool => 'minimal_site_app_pool',
+    require         => File['minimal'],
+  }
+
+  file { 'minimal':
+    ensure => 'directory',
+    path   => 'c:\\inetpub\\minimal',
+  }
 }
