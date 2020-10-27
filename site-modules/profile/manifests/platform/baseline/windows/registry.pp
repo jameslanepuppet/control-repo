14ab6ea3898d9cb0177e6547dcd1290f6206066b
jameslanepuppet <james.lane@puppet.com>
2020-10-27 18:50:16 +0000

    updated registry.pp

--- site-modules/profile/manifests/platform/baseline/windows/registry.pp
+++ site-modules/profile/manifests/platform/baseline/windows/registry.pp
@@ -2,10 +2,10 @@
 
 class profile::platform::baseline::windows::registry {
 
-  registry_value { 'Enable IE ESC for Administrators':
-    path   => 'HKEY_LOCAL_MACHINE/SOFTWARE/Microsoft/Active Setup/Installed Components/{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}',
-    ensure => present,
-  }
+ # registry_value { 'Enable IE ESC for Administrators':
+ #   path   => 'HKEY_LOCAL_MACHINE/SOFTWARE/Microsoft/Active Setup/Installed Components/{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}',
+ #   ensure => present,
+ # }
   registry_value { 'Enable Windows Event Tracker':
     path   => 'HKEY_LOCAL_MACHINE/SOFTWARE/Policies/Microsoft/Windows NT/Reliability',
     ensure => present,
