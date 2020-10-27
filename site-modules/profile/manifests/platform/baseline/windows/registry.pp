faff0fb1df530ccde0bbee72bc4c86cd4dd8e729
jameslanepuppet <james.lane@puppet.com>
2020-10-27 18:30:12 +0000

    updated registry.pp to include windows event

--- site-modules/profile/manifests/platform/baseline/windows/registry.pp
+++ site-modules/profile/manifests/platform/baseline/windows/registry.pp
@@ -5,4 +5,7 @@ class profile::platform::baseline::windows::registry {
   registry_key { 'HKEY_LOCAL_MACHINE/SOFTWARE/Microsoft/Active Setup/Installed Components/{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}':
       ensure => present,
   }
+  registry_key { 'HKEY_LOCAL_MACHINE/SOFTWARE/Policies/Microsoft/Windows NT/Reliability':
+    ensure => present,
+  }
 }
