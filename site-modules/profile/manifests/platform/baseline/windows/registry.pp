4a03ba453593bb98843eef48db6cabf2d2554bed
jameslanepuppet <james.lane@puppet.com>
2020-10-27 19:28:50 +0000

    Updated puppetfile to include puppetlabs-dism

--- site-modules/profile/manifests/platform/baseline/windows/registry.pp
+++ site-modules/profile/manifests/platform/baseline/windows/registry.pp
@@ -1,7 +1,7 @@
 #requires puppetlabs/registry
 
 class profile::platform::baseline::windows::registry {
- 
+  
   registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability':
     ensure => present,
     data  => '1',
