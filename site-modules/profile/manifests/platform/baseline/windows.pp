1732b883c3292cd5f3ce4ba88655dd8b861ae042
jameslanepuppet <james.lane@puppet.com>
2020-10-27 18:08:15 +0000

    added registry.pp  and puppetlabs registry module

--- site-modules/profile/manifests/platform/baseline/windows.pp
+++ site-modules/profile/manifests/platform/baseline/windows.pp
@@ -2,4 +2,6 @@ class profile::platform::baseline::windows {
 
   include ::profile::platform::baseline::users::windows
   include ::profile::platform::baseline::windows::directories
+  include ::profile::platform::baseline::windows::registry
+
 }
---
+++ site-modules/profile/manifests/platform/baseline/windows/registry.pp
@@ -0,0 +1,7 @@
+#requires puppetlabs/registry
+
+class profile::platform::baseline::windows::registry {
+
+registry_key { 'HKEY_LOCAL_MACHINE/SOFTWARE/Microsoft/Active Setup/Installed Components/{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}':
+    ensure => present,
+}
