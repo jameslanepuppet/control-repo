c31d18abffb7f2cda6fd373d6767fefd81a0ab3d
jameslanepuppet <james.lane@puppet.com>
2020-10-27 14:46:21 +0000

    updated directories.pp, corrected mistakes in code

--- site-modules/profile/manifests/platform/baseline/windows/directories.pp
+++ site-modules/profile/manifests/platform/baseline/windows/directories.pp
@@ -1,14 +1,15 @@
 class profile::platform::baseline::windows::directories {
 
-   file { 'c:\users\public\test':
+   file { 'c:/users/public/test':
     ensure => 'directory',
     owner  => 'Sample Windows User',
     group  => 'Administrators',
   }
   
-  acl { 'c:\users\public\test':
+  acl { 'c:/users/public/test':
     permissions => [
     { identity => 'Administrators', rights => ['read'] },
     { identity => 'Users', rights => ['read','execute'] }
+    ],
   } 
 }
