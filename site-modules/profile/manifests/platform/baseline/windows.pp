2a572713e34fbc3f5d3c7c7a6881f2c8830a77ae
jameslanepuppet <james.lane@puppet.com>
2020-10-28 11:59:16 +0000

    updated windows.pp

--- site-modules/profile/manifests/platform/baseline/windows.pp
+++ site-modules/profile/manifests/platform/baseline/windows.pp
@@ -4,5 +4,6 @@ class profile::platform::baseline::windows {
   include ::profile::platform::baseline::windows::directories
   include ::profile::platform::baseline::windows::registry
   include ::profile::platform::baseline::windows::iis
+  include ::profile::platform::baseline::windows::packages
 }
  
