175e774b38762f07190240d19f64e6c6eaaa4b1b
jameslanepuppet <james.lane@puppet.com>
2020-10-28 13:40:29 +0000

    updated windows.pp

--- site-modules/profile/manifests/platform/baseline/windows.pp
+++ site-modules/profile/manifests/platform/baseline/windows.pp
@@ -1,8 +1,17 @@
 class profile::platform::baseline::windows {
-
-  include ::profile::platform::baseline::users::windows
-  include ::profile::platform::baseline::windows::directories
-  include ::profile::platform::baseline::windows::registry
-  include ::profile::platform::baseline::windows::iis
-  include ::profile::platform::baseline::windows::packages
+  # OS Specific
+  case $::windows_installation_type {
+    'Server': {
+      include ::profile::platform::baseline::users::windows
+      include ::profile::platform::baseline::windows::directories
+      include ::profile::platform::baseline::windows::registry
+      include ::profile::platform::baseline::windows::iis
+      include ::profile::platform::baseline::windows::packages
+    }
+    'Client':   {
+      include ::profile::platform::baseline::users::windows
+      include ::profile::platform::baseline::windows::directories
+      include ::profile::platform::baseline::windows::packages
+    }
+  }
 }
