ac1c468635de38c83f817185c9958b47de7f76db
James Lane <61185185+jameslanepuppet@users.noreply.github.com>
2020-10-30 09:09:45 +0000

    Update registry.pp

--- site-modules/profile/manifests/platform/baseline/windows/registry.pp
+++ site-modules/profile/manifests/platform/baseline/windows/registry.pp
@@ -4,13 +4,13 @@ class profile::platform::baseline::windows::registry {
   
   registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability':
     ensure => present,
-    data  => '1',
-    type  => 'dword',
+    data   => '1',
+    type   => 'dword',
   }
 
  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}':
     ensure => present,
-    data  => '1',
-    type  => 'dword',
+    data   => '1',
+    type   => 'dword',
   }
 }
