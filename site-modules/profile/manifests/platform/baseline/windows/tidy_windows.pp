e8876158f594a4bb36b93e7e01288180f4303156
James Lane <61185185+jameslanepuppet@users.noreply.github.com>
2020-11-26 13:44:08 +0000

    Update tidy_windows.pp
    updated file

--- site-modules/profile/manifests/platform/baseline/windows/tidy_windows.pp
+++ site-modules/profile/manifests/platform/baseline/windows/tidy_windows.pp
@@ -3,16 +3,16 @@ class  profile::platform::baseline::windows::tidy_windows{
   # Always remove the given file
   tidy { 'c:/example.log': }
 
-  # remove all files from /tmp/dropbox
-  tidy { 'clean_dropbox':
-    path    => 'c:/Temp',
+  # remove all files from c:/tmp
+  tidy { 'clean_tmp':
+    path    => 'c:/Tmp',
     recurse => 1,
   }
 
-  # remove all files, in /tmp/image_cache,
+  # remove all files, in c:/tmp_1_week
   # that are older than 1 week
   tidy { 'prune_old_caches':
-    path    => 'c:/temp_1_week',
+    path    => 'c:/tmp_1_week',
     age     => '1w',
     recurse => 1,
   }
